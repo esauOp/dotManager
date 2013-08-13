bbbed2f33b0f68a3d43510336335e12d84bd50ca
esauOp <monkeycoss@gmail.com>
2013-08-13 18:44:11 -0500

    New Rails Application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DotManager::Application.config.secret_key_base = 'aa4f549207034c2cf1a2a1ef5b8bf58f19a0cd33bdc0c80548341f4dff63cb2751326e3d36cb40f384dfc3a17f65aad46e4351ad8abf4c475ded152d943d94e4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DotManager::Application.config.session_store :cookie_store, key: '_dotManager_session'
