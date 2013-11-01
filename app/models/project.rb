0cc0841e3d96b5042f4ee77dee9368940b924050
esauOp <monkeycoss@gmail.com>
2013-10-31 19:34:08 -0600

    Project->tasks order by desc

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-	has_many :tasks, dependent: :destroy
+	has_many :tasks, :order => "id DESC", dependent: :destroy
 	belongs_to :cliente
 	# belongs_to :usuario
 	has_and_belongs_to_many :usuarios	
