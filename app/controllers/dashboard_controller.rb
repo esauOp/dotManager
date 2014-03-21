018210df5d7edd123758c0f799420f5292bd8105
esauOp <monkeycoss@gmail.com>
2014-03-20 20:07:23 -0600

    Expoting to CSV, step 1...

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,5 +3,18 @@ class DashboardController < ApplicationController
   	# if !current_usuario
   	# 	redirect_to(new_usuario_session_path)
   	# end
+
+  	if current_usuario.admin      
+      @tasks = Task.all
+    else
+    #@projects = Project.all(:include => :usuarios, :conditions => ["usuarios.id = ?", current_usuario.id])     
+      @tasks = Task.includes(:usuarios).where(usuarios: { id:current_usuario.id })
+    end
+
+    respond_to do |format|
+      format.html
+      format.csv  { send_data @tasks.as_csv }
+      format.xls  #{ send_data @projects.as_csv(col_sep: "\t") }
+    end
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -22,4 +22,13 @@ class Task < ActiveRecord::Base
     }
     
   end
+
+  def self.as_csv(options = {})
+    CSV.generate(options) do |csv|
+      csv << column_names
+      all.each do |item|
+        csv << item.attributes.values_at(*column_names)
+      end
+    end
+  end
 end
